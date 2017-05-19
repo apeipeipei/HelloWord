11b792b4484f8d8a2a7ba270dc6f74df219a0f93
czy <516790759@qq.com>
2017-05-19 10:59:30 +0800

    Spring
    用于学习

---
+++ SpringWeb/src/com/czy/spring/HelloWordBean.java
@@ -0,0 +1,7 @@
+package com.czy.spring;
+
+public class HelloWordBean {
+  public String sayHello(){
+    return "Hello World";
+  }
+}
---
+++ SpringWeb/src/com/springinaction/Knight/DamselRescuingKnight.java
@@ -0,0 +1,5 @@
+package com.springinaction.Knight;
+
+public class DamselRescuingKnight {
+
+}
