0ba9c9d4cf912511e3a9102f519361b6fb0e5749
czyhome <516790759@qq.com>
2017-05-20 21:30:23 +0800

    使用Beanfactory获取Bean

--- Knife/src/com/wenger/test/test.java
+++ Knife/src/com/wenger/test/test.java
@@ -2,17 +2,27 @@ package com.wenger.test;
 
 
 import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
 import com.wenger.demo.UserService;
 
-public class text {
+public class test {
 	@Test
-	public void userText(){
+	public void userTest(){
 		//获取spring配置文件(获取工厂)
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		//通过工厂获取bean
 		UserService user = (UserService) context.getBean("us");
 		user.sayHello();
+	}
+	@Test
+	public void userTest1(){
+		BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("applicationContext.xml"));
+		UserService service  = (UserService) beanFactory.getBean("us");
+		service.sayHello();
 	}
 }
